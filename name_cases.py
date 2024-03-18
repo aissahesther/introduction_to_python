def05366d2f06fb6fe68d916cf74253f9e0d9afb
Esther AISSAH <135969404+aissahesther@users.noreply.github.com>
2024-03-18 10:17:26 +0100

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,53 @@
+
+#____print a personal Message
+
+name = "Alia"
+
+print(f"hello {name}, would you like to learn some python Today?")
+
+
+#_____name cases
+
+#print in lowercase
+
+print(name.lower())
+
+
+#print in uppercase 
+
+print (name.upper())
+
+#print in title case
+
+print (name.title())
+
+#_____Famous Quote 
+
+
+name = "Francis Bacon"
+quote ='"Knowledge is power."'
+
+print (f"Sir {name} once said {quote}")
+
+
+#_____famous quote 2
+
+message = f"sir {name} once said {quote}"
+
+print (message)
+
+
+#________stripping names 
+
+name = "  esther alia  "
+
+print (name)
+
+print (name.lstrip())
+
+print (name.rstrip())
+
+print (name.strip())
+
+
+
