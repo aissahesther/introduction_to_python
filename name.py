01ae8e4589ff3f69fc8fd212a640f46042fef015
Esther AISSAH <135969404+aissahesther@users.noreply.github.com>
2024-03-18 06:08:23 +0100

    Add files via upload

---
+++ name.py
@@ -0,0 +1,17 @@
+#Working with string
+
+
+name = "ada lovelace"
+
+#make the title() method act on the variable name
+#The title() method changes each word to title case, where each word begins with a capital letter. 
+
+print(name.title())
+
+#make upper() method act on the variable name
+
+print (name.upper())
+
+#make lower() method act on the variable name
+
+print (name.lower())
