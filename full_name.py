6648e8be3304b29749fd6aeff09255e6282df827
Esther AISSAH <135969404+aissahesther@users.noreply.github.com>
2024-03-18 06:22:48 +0100

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,20 @@
+#using variables in string
+
+
+#exemple
+#you want 2 variables to represent a first name and a last name respectively
+
+#and then want to combine those values to display someone's full name
+
+
+first_name ="esther" 
+last_name = "aissah"
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+#make the title() method act on the variable name
+
+print(f"hello, {full_name.title()}!")
+
