8fe8836a1b6371b18fca83a5f89eef7a7a6494be
Esther AISSAH <135969404+aissahesther@users.noreply.github.com>
2024-03-18 09:59:47 +0100

    Add files via upload

---
+++ apostrophe.py
@@ -0,0 +1,11 @@
+
+#avoiding syntax errors with Strings
+
+#right 
+message = "One of Python's strengths is its divers community"
+print (message)
+
+#wrong
+#message= 'one of Python's strengths is its diverse community'
+
+#print(message)
